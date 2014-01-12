940f0aebb37484c173664c08ee6c3425e8ec156d
sorgal <cheeerny@rambler.ru>
2014-01-12 21:10:03 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nightzone::Application.config.secret_key_base = '3e69910e824be6d0e56d44ec33cbd00cc0f6353d0264ad137a8c0da8ee11aae2432cb403b8317baf6d9eb3cda9fe788cd7bfba8834bc751a84f8d0ced89019eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nightzone::Application.config.session_store :cookie_store, key: '_nightzone_session'
