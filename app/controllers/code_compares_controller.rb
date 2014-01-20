a2d7a016a4e01ee0b7e46bbef648831cd6e459ff
sorgal <cheeerny@rambler.ru>
2014-01-20 10:11:59 +0400

    changes berfore switch branch

--- app/controllers/code_compares_controller.rb
+++ app/controllers/code_compares_controller.rb
@@ -7,8 +7,6 @@ class CodeComparesController < ApplicationController
   def create
     @task = current_user.tasks.last
     @game = @task.game
-
-    notice = ""
     notice = @game.process(current_user, @task, params[:try_text])
 
     respond_to do |format|
@@ -44,42 +42,6 @@ class CodeComparesController < ApplicationController
       @game = @task.game
       redirect_to game_path(id: @game.game)
     end
-  end
-
-  def pass_matching(text)
-    if User.find(current_user.id).valid_password?(text)
-      return true
-    else
-      return false
-    end
-  end
-
-  def next_task(points)
-    if has_more_tasks?
-      assign_next_task(points)
-      "Next task assigned"
-    else
-      end_game
-      "Game completed"
-    end
-  end
-
-  def has_more_tasks?
-    untaken_tasks.any?
-  end
-
-  def assign_next_task(points)
-    current_user.user_tasks.last.update_attribute(:result, points)
-    current_user.user_tasks.create(task: untaken_tasks.first)
-  end
-
-  def end_game
-    current_user.user_games.current.first.update_attribute(:state, UserGame::COMPLETED)
-  end
-
-  def untaken_tasks
-    completed_ids = @game.tasks.for_user(current_user.id).pluck(:id)
-    @untaken_tasks ||= @game.tasks.where('`tasks`.`id` NOT IN (?)', completed_ids.join(',') )
   end
 
 end
