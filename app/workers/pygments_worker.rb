c7151773e64729d863fbcb1f20816c75cdb85e7b
sorgal <cheeerny@rambler.ru>
2014-02-03 13:23:53 +0400

    faye with sidekiq

--- app/workers/pygments_worker.rb
+++ app/workers/pygments_worker.rb
@@ -3,6 +3,8 @@ class PygmentsWorker
   sidekiq_options queue: "high"
 
   def perform
-    puts "It works"
+    message = {:channel => '/games', :data => "alert('It works!')"}
+    uri = URI.parse("http://localhost:9292/faye")
+    Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
